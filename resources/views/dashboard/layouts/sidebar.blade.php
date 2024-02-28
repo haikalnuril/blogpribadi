1354b9812dc8e060c36d491893c8690bd7f5143d
haikalnuril <haikal.nuril23@gmail.com>
2024-02-28 21:46:47 +0700

    feat: Creating Post Category section

--- resources/views/dashboard/layouts/sidebar.blade.php
+++ resources/views/dashboard/layouts/sidebar.blade.php
@@ -7,18 +7,31 @@
         <div class="offcanvas-body d-md-flex flex-column p-0 pt-lg-3 overflow-y-auto">
           <ul class="nav flex-column">
             <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard') ? 'active' : '' }}" href="/dashboard">
+              <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard/*') ? 'active' : '' }}" href="/dashboard">
                 <i data-feather="home"></i>
                 Dashboard
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard') ? 'active' : '' }}" href="/dashboard/posts">
+              <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard/posts*') ? 'active' : '' }}" href="/dashboard/posts">
                 <i data-feather="file-text"></i>
                 My Posts
               </a>
             </li>
           </ul>
+
+          <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-muted">
+            <span>Administrator</span>
+          </h6>
+          <ul class="nav flex-column">
+            <li class="nav-item">
+            <a class="nav-link d-flex align-items-center gap-2 {{ Request::is('dashboard/categories*') ? 'active' : '' }}" href="/dashboard/categories">
+                <i data-feather="grid"></i>
+                Post Categories
+            </a>  
+            </li>
+          </ul>
+
         </div>
       </div>
     </div>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminCategoryController;
 use App\Models\User;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
@@ -93,3 +94,5 @@ route::get('/dashboard', function(){
 
 route::get('/dashboard/posts/checkSlug', [DashboardPostController::class, 'checkSlug'])->middleware('auth');
 route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
+
+route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')->middleware('auth');
