1cedd6b8eb6fd050f97a746ff87f3e5c21e18b20
haikalnuril <haikal.nuril23@gmail.com>
2024-03-19 22:12:27 +0700

    adding and testing tailwind css

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,4 +5,7 @@
     <h3>{{ $name }}</h3>
     <p>{{ $email }}</p>     
     <img src="img/{{ $image}}" alt="{{ $name }}" width="300" class="img-thumbnail rounded">
+    <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
 @endsection
